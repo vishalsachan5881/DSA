befabb3b202d2808710c9123b252ace40db03352
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2021-12-29 03:49:32 -0800

    Time: 0 ms (100.00%), Space: 37.6 MB (51.02%) - LeetHub

---
+++ remove-element/remove-element.java
@@ -0,0 +1,15 @@
+class Solution {
+   public int removeElement(int[] A, int elem) {
+   int m = 0;    
+   for(int i = 0; i < A.length; i++){
+       
+       if(A[i] != elem){
+           A[m] = A[i];
+           m++;
+       }
+   }
+   
+   return m;
+}
+
+}
