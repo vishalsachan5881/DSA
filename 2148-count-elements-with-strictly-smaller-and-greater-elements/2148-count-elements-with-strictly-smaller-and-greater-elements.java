7a32a388b267d42be91f9f7bcf953649972b254f
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2022-01-29 12:05:47 -0800

    Time: 6 ms (13.95%), Space: 43.2 MB (5.03%) - LeetHub

---
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countElements(int[] nums) {
+        int n = nums.length;
+        TreeMap<Integer , Integer> tm = new TreeMap<>();
+        for(int i = 0; i<n; i++){
+            if(tm.containsKey(nums[i])) tm.put(nums[i] , tm.get(nums[i])+1);
+            else tm.put(nums[i] , 1);
+        }
+        int ans =  n - tm.get(tm.firstKey()) - tm.get(tm.lastKey());
+        if(ans < 0) return 0;
+        else return ans;
+        
+    }
+}
