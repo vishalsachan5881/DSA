7c2cba68a937ca6b412303d1edc5503fa25be1b5
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2022-02-15 08:14:39 -0800

    Time: 2 ms (64.57%), Space: 50.9 MB (25.09%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n  = nums[0];
+        for(int i = 1 ; i < nums.length ; i++)
+            n=n^nums[i];
+        
+        return n ; 
+    }
+}
