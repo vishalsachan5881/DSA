6669bab64948ecc10a2661e56aadfb62ae4cc13c
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2022-03-22 16:06:12 +0530

    Time: 1 ms (93.07%), Space: 42 MB (86.66%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -11,9 +11,12 @@ class Solution {
         if(close !=0 && open < close)
             fn(open, close-1, ansf+')' ,ans);
     }
+
+            
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
         fn(n,n,"",ans);
         return ans;
     }
-}
+
+    }
