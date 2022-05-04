19dfb179b9448f3f528165675469b9f53f812e01
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2022-05-04 12:19:24 -0700

    Time: 23 ms (75.38%), Space: 70 MB (57.83%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,34 +1,17 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i = 0 ; i<nums.length ; i++){
-            if(hm.containsKey(nums[i]))
-                hm.put(nums[i],hm.get(nums[i])+1);
-            else
-                hm.put(nums[i],1);
-        }
-        System.out.println(hm);
-        int ans = 0;
-        for(var v : hm.keySet()){
-            if(hm.containsKey(k-v)) {
-
-                if(k-v == v) {
-                    ans += hm.get(v)/2;
-                    hm.put(v, hm.get(v) % 2);
-
-                }
+        Arrays.sort(nums);
+        int n = nums.length;
+        int l = 0 , r = n-1,ans = 0;
+        while(l<r){
+            if(nums[r] + nums[l] == k)
+                {l++;r--;ans++;}
+            else if(nums[r]+nums[l]>k)
+                r--;
                 else
-                {
-                    int p = Math.min(hm.get(v), hm.get(k - v));
-                    hm.put(v, hm.get(v) - p);
-                    hm.put(k - v, hm.get(k - v) - p);
-
-                    ans += p;
-                }
-            }
-
+                    l++;
+        
         }
-        // System.out.println(ans);
         return ans;
     }
 }
